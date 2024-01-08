3a8bbdc31c0c531593505eaa261570cc196dbd57
Senexdevops <kuzmi.deni@gmail.com>
2024-01-08 18:43:06 +0500

    6.0.7. Массивы в тестах. [#226789 #471413]

--- src/test/java/ru/job4j/array/SquareTest.java
+++ src/test/java/ru/job4j/array/SquareTest.java
@@ -3,7 +3,6 @@ package ru.job4j.array;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SuppressWarnings("checkstyle:EmptyLineSeparator")
 class SquareTest {
     @Test
     void whenBound3Then014() {
